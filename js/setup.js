6710381014b31e32acc6512a2cd302f5f45170b3
Maria2323 <manehka_ilu@mail.ru>
2020-06-04 12:15:48 +0300

    Добавляет функцию для создания волшебников

--- js/setup.js
+++ js/setup.js
@@ -3,7 +3,7 @@
 var userDialog = document.querySelector('.setup');
 userDialog.classList.remove('hidden');
 
-var firstNames = ['Иван', 'Хуан Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'];
+var firstNames = ['Иван ', 'Хуан Себастьян ', 'Мария ', 'Кристоф ', 'Виктор ', 'Юлия ', 'Люпита ', 'Вашингтон '];
 var lastNames = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
 var coatColors = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
 var eyesColors = ['black', 'red', 'blue', 'yellow', 'green'];
@@ -11,12 +11,16 @@ var eyesColors = ['black', 'red', 'blue', 'yellow', 'green'];
 var wizardCount = 4;
 var characters = [];
 
-for (var i = 0; i < wizardCount; i++) {
-  characters.push({
-    name: firstNames[Math.round(Math.random() * firstNames.length - 1)] + lastNames[Math.round(Math.random() * lastNames.length - 1)],
-    coatColor: coatColors[Math.round(Math.random() * coatColors.length - 1)],
-    eyesColor: eyesColors[Math.round(Math.random() * eyesColors.length - 1)]});
-}
+var createWizard = function (objects, count) {
+  for (var i = 0; i < count; i++) {
+    objects.push({
+      name: firstNames[Math.round(Math.random() * (firstNames.length - 1))] + lastNames[Math.round(Math.random() * (lastNames.length - 1))],
+      coatColor: coatColors[Math.round(Math.random() * (coatColors.length - 1))],
+      eyesColor: eyesColors[Math.round(Math.random() * (eyesColors.length - 1))]});
+  }
+  return objects;
+};
+createWizard(characters, wizardCount);
 
 var similarListElement = document.querySelector('.setup-similar-list');
 var similarWizardTemplate = document.querySelector('#similar-wizard-template').content.querySelector('.setup-similar-item');
