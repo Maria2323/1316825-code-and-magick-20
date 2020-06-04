79cf0993ac00445ba90bdbf3b3e1c6304bc8d231
Maria2323 <manehka_ilu@mail.ru>
2020-06-04 12:57:53 +0300

    Корректирует код, добавляет функция для создания рандомно объектов

--- js/setup.js
+++ js/setup.js
@@ -1,26 +1,27 @@
 'use strict';
 
+var FIRST_NAMES = ['Иван ', 'Хуан Себастьян ', 'Мария ', 'Кристоф ', 'Виктор ', 'Юлия ', 'Люпита ', 'Вашингтон '];
+var LAST_NAMES = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
+var COAT_COLORS = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
+var EYES_COLORS = ['black', 'red', 'blue', 'yellow', 'green'];
+
 var userDialog = document.querySelector('.setup');
 userDialog.classList.remove('hidden');
-
-var firstNames = ['Иван ', 'Хуан Себастьян ', 'Мария ', 'Кристоф ', 'Виктор ', 'Юлия ', 'Люпита ', 'Вашингтон '];
-var lastNames = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
-var coatColors = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
-var eyesColors = ['black', 'red', 'blue', 'yellow', 'green'];
 
 var wizardCount = 4;
 var characters = [];
 
-var createWizard = function (objects, count) {
-  for (var i = 0; i < count; i++) {
-    objects.push({
-      name: firstNames[Math.round(Math.random() * (firstNames.length - 1))] + lastNames[Math.round(Math.random() * (lastNames.length - 1))],
-      coatColor: coatColors[Math.round(Math.random() * (coatColors.length - 1))],
-      eyesColor: eyesColors[Math.round(Math.random() * (eyesColors.length - 1))]});
-  }
-  return objects;
+var getRandomElement = function (characteristic) {
+  return characteristic[Math.round(Math.random() * (characteristic.length - 1))];
 };
-createWizard(characters, wizardCount);
+
+var createWizards = function () {
+  return {
+    name: getRandomElement(FIRST_NAMES) + getRandomElement(LAST_NAMES),
+    coatColor: getRandomElement(COAT_COLORS),
+    eyesColor: getRandomElement(EYES_COLORS)
+  };
+};
 
 var similarListElement = document.querySelector('.setup-similar-list');
 var similarWizardTemplate = document.querySelector('#similar-wizard-template').content.querySelector('.setup-similar-item');
@@ -42,6 +43,10 @@ var addCharacters = function (list) {
   }
   list.appendChild(fragment);
 };
+for (var i = 0; i < wizardCount; i++) {
+  characters.push(createWizards());
+};
+
 addCharacters(similarListElement);
 
 document.querySelector('.setup-similar').classList.remove('hidden');
